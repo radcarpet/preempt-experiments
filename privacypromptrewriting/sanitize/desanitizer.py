93ee08ff30dd003e90456a877475baa5ec6c6e63
radcarpet <divyamanshumaan2023@gmail.com>
2024-10-12 21:21:53 -0500

    Update desanitizer.py

--- privacypromptrewriting/sanitize/desanitizer.py
+++ privacypromptrewriting/sanitize/desanitizer.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import asyncio
 import json
 
+import sys
+sys.path.append("../..")
 from privacypromptrewriting.sanitize.desanitizer_agent import (
     DesanitizerAgent,
     DesanitizerAgentConfig,
